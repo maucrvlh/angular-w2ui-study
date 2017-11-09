7347c94b5e79aba9e029c6fb6a8c058d31e6e4bc
Mauricio Marinho <mauriciocmarinho@gmail.com>
2017-11-08 21:13:58 -0400

    tests

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +9,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  hero:Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 
   constructor() { }
 
