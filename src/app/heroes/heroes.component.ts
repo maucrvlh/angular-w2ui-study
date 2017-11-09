fcaff9538c1cc0cc94f30ec88958f3eadf7773b3
maucrvlh <mauricio.marinho@tjam.jus.br>
2017-11-09 14:00:54 -0400

    +tests

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,22 +3,24 @@ import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+    heroes = HEROES;
 
-  hero:Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+    selectedHero: Hero;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+    }
+
+    onSelect(hero:Hero): void {
+        this.selectedHero = hero;
+    }
 
 }
